9edacd95f9ad9654841b3a6b217cc83692b2c011
Kan Adachi <kadachi1023@gmail.com>
2017-09-27 16:52:27 -0700

    [feat] adds rest-server for client to post code-to-run

---
+++ rest-server/src/index.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import axios from 'axios';
+import cors from 'cors';
+
+import log from './lib/log';
+
+const app = express();
+const port = 4990 || process.env.PORT;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/run', async function(req, res) {
+  const { code } = req.body;
+  if (!code) {
+    return res.json({
+      success: false,
+      message: 'code property on req.body is required',
+    });
+  }
+
+  try {
+    const resp = await axios.post(`${process.env.CODERUNNER_SERVICE_URL}/submit-code`, {
+      code,
+    });
+    let stdout = resp.data;
+    res.json({
+      success: true,
+      stdout,
+    });
+  } catch (e) {
+    log('error posting to coderunner service. e = ', e);
+    res.status(400).json({
+      success: false,
+      message: 'error running your code.'
+    });
+  }
+});
+
+app.listen(port, () => log(`rest-server listening on port ${port}`));
