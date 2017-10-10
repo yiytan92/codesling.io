6d9bc01003a5328e9e521b8ef6be27a50442f4d3
Justin Kang <j6k@Justins-MacBook-Pro.local>
2017-10-10 14:02:01 -0700

    [ops] Working tests Some inconsistencies with consecutive runs with the test

--- socket-server/tests/clientEvents.test.js
+++ socket-server/tests/clientEvents.test.js
@@ -1,11 +1,12 @@
 const ioClient = require('socket.io-client/dist/socket.io.js');
+const { after } = require('lodash');
 
 const clientEvents = require('../src/clientEvents');
 
 require('dotenv').config();
 require('dotenv').load();
 
-describe('All events should be functions', () => {
+describe('All client events should be functions', () => {
   // Expects all clientEvents to be functions
   test('client.ready should be a function', () => {
     expect(typeof clientEvents.default['client.ready']).toBe('function');
@@ -23,34 +24,40 @@ describe('All events should be functions', () => {
 describe('Client interactions', () => {
   const client1 = ioClient.connect(process.env.TEST_SOCKET_SERVER_PORT);
   const client2 = ioClient.connect(process.env.TEST_SOCKET_SERVER_PORT);
-  // beforeEach(() => {
-  beforeAll(() => {
+  beforeAll(done => {
+    done = after(2, done);
     client1.on('connect', () => {
-      client1.emit('client.ready');
+      done();
     });
     client2.on('connect', () => {
-      client2.emit('client.ready');
+      done();
     });
   });
-  // afterEach(() => {
-  afterAll(() => {
-    client1.disconnect();
-    client2.disconnect();
+  afterAll(async (done) => {
+  // afterAll(done => {
+    await client1.disconnect();
+    await client2.disconnect();
+    done();
+    // setTimeout(() => process.exit(), 1000)
   });
   // Expects people to be able to connect to the socket-server
-  test('Should add a new user to a room and receive the same text', (done) => {
+  test('Should add a new user to a room and receive the same text', done => {
+    done = after(2, done);
     expect.assertions(2);
-    client1.on('server.initialState', (msg) => {
-      const obj = { text: '// your code here' };
-      expect(JSON.stringify(msg)).toBe(JSON.stringify(obj));
-      client2.on('server.initialState', msg => {
-        expect(JSON.stringify(msg)).toBe(JSON.stringify(obj));
-        done();
-      });
+    const obj = { text: '// your code here' };
+    client1.on('server.initialState', text => {
+      expect(JSON.stringify(text)).toBe(JSON.stringify(obj));
+      done();
     });
+    client2.on('server.initialState', text => {
+      expect(JSON.stringify(text)).toBe(JSON.stringify(obj));
+      done();
+    });
+    client1.emit('client.ready');
+    client2.emit('client.ready');
   });
   // Expects clients to be able to connect with one another
-  test('Should be able to hear emissions from other clients', (done) => {
+  test('Should be able to hear emissions from other clients', done => {
     expect.assertions(1);
     client2.on('server.changed', payload => {
       expect(payload.text).toBe('console.log(2)');
@@ -59,11 +66,11 @@ describe('Client interactions', () => {
     client1.emit('client.update', { text: 'console.log(2)'});
   });
   // // Expects clients to disconnect
-  test('Client should have disconnected', () => {
+  test('Client should have disconnected', done => {
     expect.assertions(1);
     client1.on('server.leave', () => {
-      console.log('we shoudl be here')
       expect(1).toBe(1);
+      done();
     });
     client1.emit('client.disconnect')
   });
