40a828d0de21ebf22f7b764fac69eb182b95c064
Kan Adachi <kadachi1023@gmail.com>
2017-10-16 21:45:35 -0700

    [fix] fixes async issue with rerun of socket tests

--- socket-server/tests/sockets.test.js
+++ socket-server/tests/sockets.test.js
@@ -1,5 +1,7 @@
 const ioClient = require('socket.io-client/dist/socket.io.js');
 const { after } = require('lodash');
+const delay = require('delay');
+const randomId = require('random-id');
 
 const clientEvents = require('../src/clientEvents');
 
@@ -7,10 +9,15 @@ require('dotenv').config();
 require('dotenv').load();
 
 describe('Client interactions', () => {
-  const client1 = ioClient.connect(`${process.env.HOST}:${process.env.PORT}`);
-  const client2 = ioClient.connect(`${process.env.HOST}:${process.env.PORT}`);
-  beforeAll(done => {
+  let client1;
+  let client2;
+  let roomId = randomId(10);
+
+  beforeAll((done) => {
     done = after(2, done);
+    const query = { query: `roomId=${roomId}` };
+    client1 = ioClient.connect(`${process.env.HOST}:${process.env.PORT}`, query);
+    client2 = ioClient.connect(`${process.env.HOST}:${process.env.PORT}`, query);
     client1.on('connect', () => {
       done();
     });
@@ -18,35 +25,47 @@ describe('Client interactions', () => {
       done();
     });
   });
+
   afterAll(async (done) => {
-    await client1.disconnect();
-    await client2.disconnect();
+    client1.disconnect();
+    client2.disconnect();
+    await delay(300);
     done();
   });
 
   // Expects people to be able to connect to the socket-server
-  test('Should add a new user to a room and receive the same text', done => {
+  test('Should add a new user to a room and receive the same text', async (done) => {
     done = after(2, done);
-    expect.assertions(2);
-    const obj = { text: '// your code here' };
-    client1.on('server.initialState', text => {
-      expect(JSON.stringify(text)).toBe(JSON.stringify(obj));
+    expect.assertions(4);
+    const fixture = { text: '// your code here' };
+    const handler = (text) => {
+      try {
+        expect(JSON.stringify(text)).toBe(JSON.stringify(fixture));
+        // make sure real test above does not throw
+        expect(true).toBe(true);
+      } catch (e) {
+        console.log(e.toString());
+      }
       done();
-    });
-    client2.on('server.initialState', text => {
-      expect(JSON.stringify(text)).toBe(JSON.stringify(obj));
-      done();
-    });
-    console.log('emitting');
+    };
+
+    client1.on('server.initialState', handler);
+    client2.on('server.initialState', handler);
+
     client1.emit('client.ready');
     client2.emit('client.ready');
   });
 
   // Expects clients to be able to connect with one another
-  test('Should be able to hear emissions from other clients', done => {
-    expect.assertions(1);
-    client2.on('server.changed', payload => {
-      expect(payload.text).toBe('console.log(2)');
+  test('Should be able to hear emissions from other clients', (done) => {
+    expect.assertions(2);
+    client2.on('server.changed', (payload) => {
+      try {
+        expect(payload.text).toBe('console.log(2)');
+        expect(true).toBe(true);
+      } catch (e) {
+        console.log(e.toString());
+      }
       done();
     });
     client1.emit('client.update', { text: 'console.log(2)'});
@@ -56,9 +75,10 @@ describe('Client interactions', () => {
   test('Client should have disconnected', done => {
     expect.assertions(1);
     client1.on('server.leave', () => {
-      expect(1).toBe(1);
+      // ensures server.leave is heard for other room clients
+      expect(true).toBe(true);
       done();
     });
-    client1.emit('client.disconnect');
+    client2.emit('client.disconnect');
   });
 });
