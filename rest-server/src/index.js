36ec5d7fc50d4f9e88ed32384344160eccf0f61c
Justin Kang <j6k@Justins-MacBook-Pro.local>
2017-10-23 14:25:47 -0700

    [ops] - bluebird to promisify mongoose

--- rest-server/src/index.js
+++ rest-server/src/index.js
@@ -4,12 +4,16 @@ import axios from 'axios';
 import cors from 'cors';
 
 import log from './lib/log';
+import db from './database';
+import router from './database/routes';
 
 const app = express();
 const port = 4990 || process.env.PORT;
 
 app.use(bodyParser.json());
 app.use(cors());
+
+app.use('/api', router);
 
 app.post('/run', async (req, res) => {
   const { code } = req.body;
