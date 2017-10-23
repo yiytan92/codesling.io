15ee84c93eecb2b4a1de9279e57dc3fc0f1a1fb1
Justin Kang <j6k@Justins-MacBook-Pro.local>
2017-10-23 14:26:50 -0700

    [ops] - mongodb model and controller for users - just accepting a username and password for now

---
+++ rest-server/src/database/controllers/userController.js
@@ -0,0 +1,67 @@
+import mongoose from 'mongoose';
+import bluebird from 'bluebird';
+
+import { User } from '../models/user';
+import log from '../../lib/log';
+
+mongoose.Promise = bluebird;
+
+module.exports = {
+  userFetch: async (req, res) => {
+    try {
+      const user = await User.findById(req.params.id);
+      res.status(200).send(user);
+    } catch (err) {
+      res.status(400).send(err);
+    }
+  },
+  userPost: (req, res) => {
+    const newUser = new User(req.body);
+    try {
+      newUser.save((err, userCreated) => {
+        if (err) {
+          log('Error saving user ', err);
+        } else {
+          log('Successfully saved user to the db');
+          res.status(200).send(userCreated);
+        }
+      });
+    } catch (err) {
+      res.send(400).send(err);
+    }
+  },
+  userUpdate: (req, res) => {
+    try {
+      User.findById(req.params.id, (err, user) => {
+        if (err) {
+          log('Error in userUpdate ', err);
+        } else {
+          user.username = req.body.username;
+          user.password = req.body.password;
+          user.save((error, updatedUser) => {
+            if (error) {
+              log('Error updating user ', error);
+            } else {
+              res.status(200).send(updatedUser);
+            }
+          });
+        }
+      });
+    } catch (err) {
+      res.status(400).send(err);
+    }
+  },
+  userDelete: (req, res) => {
+    try {
+      User.findByIdAndRemove(req.params.id, (err, user) => {
+        const response = {
+          message: 'User has been successfully deleted',
+          id: user.id,
+        };
+        res.status(200).send(response);
+      });
+    } catch (err) {
+      res.send(400).send(err);
+    }
+  },
+};
