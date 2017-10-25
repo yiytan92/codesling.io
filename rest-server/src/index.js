54f338af2b6bcd85b3d800e6427a6b5c9d88e651
Justin Kang <j6k@Justins-MacBook-Pro.local>
2017-10-25 10:02:17 -0700

    [ops]
    - code runner route in different file now
    - route definition in routes/index.js

--- rest-server/src/index.js
+++ rest-server/src/index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import axios from 'axios';
 import cors from 'cors';
 
 import log from './lib/log';
@@ -14,32 +13,5 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use('/api', router);
-
-app.post('/run', async (req, res) => {
-  const { code } = req.body;
-  if (!code) {
-    return res.status(400).json({
-      success: false,
-      message: 'code property on req.body is required',
-    });
-  }
-
-  try {
-    const resp = await axios.post(`${process.env.CODERUNNER_SERVICE_URL}/submit-code`, {
-      code,
-    });
-    const stdout = resp.data;
-    return res.json({
-      success: true,
-      stdout,
-    });
-  } catch (e) {
-    log('error posting to coderunner service. e = ', e);
-    return res.status(400).json({
-      success: false,
-      message: 'error running your code.',
-    });
-  }
-});
 
 app.listen(port, () => log(`rest-server listening on port ${port}`));
