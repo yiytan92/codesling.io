9bf4da267938734de71680876552b5dab041d6b3
Justin Kang <j6k@Justins-MacBook-Pro.local>
2017-11-08 11:13:07 -0800

    [ops] - rooms are now slings - room -> sling in all variables and functions

--- rest-server/src/database/index.js
+++ rest-server/src/database/index.js
@@ -3,12 +3,18 @@ import log from '../lib/log';
 
 const uri = `${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB_NAME}`;
 
-mongoose.connect(uri, (err) => {
-  if (err) {
-    log('Error in mongodb connection ', err);
-  } else {
-    log('Successfully connected to mongodb');
-  }
+mongoose.connect(uri);
+
+mongoose.connection.on('connected', () => {  
+  log('Mongoose default connection open to mongodb://127.0.0.1:27017/codesling');
+});
+
+mongoose.connection.on('error', (error) => {
+  log('Mongoose default connection error: ', error);
+});
+
+mongoose.connection.on('disconnected', () => {
+  log('Mongoose default connection disconnected');
 });
 
 const db = mongoose.connection;
--- rest-server/src/middleware/authentication.js
+++ rest-server/src/middleware/authentication.js
@@ -1,5 +1,49 @@
 import jwt from 'jsonwebtoken';
+import log from '../lib/log';
 
-export const authenticate = (req, res, next) => {
-  
+export const generateToken = ({ username, id }) => {
+  const token = {};
+
+  token.accessToken = jwt.sign({
+    exp: Math.floor(Date.now() / 1000) + (60 * 60),
+    username,
+    id,
+  }, process.env.TOKEN_SECRET);
+
+  // token.refreshToken = jwt.sign({
+  //   username,
+  //   id,
+  // }, process.env.TOKEN_SECRET);
+
+  return token;
+};
+
+export const refreshToken = (req, res, next) => {
+  try {
+    const token = generateToken(req.body.token);
+    next(token);
+  } catch (error) {
+    log('Error refreshing token');
+    next(error);
+  }
+};
+
+export const verifyUserWithJWT = (req, res, next) => {
+  try {
+    jwt.verify(req.body.token, process.env.TOKEN_SECRET);
+    log('user authenticated');
+    next();
+  } catch (error) {
+    log('user not authenticated');
+  }
+};
+
+export const logout = async (req, res, next) => {
+  const { accessToken } = req.body;
+  try {
+    // delete the token
+  } catch (error) {
+    log('Error logging out');
+    next(error);
+  }
 };
