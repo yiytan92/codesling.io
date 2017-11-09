78ace8b551d7c0f8058eb9f54972f46a08b7ef83
Daniel Chong <danchong625@gmail.com>
2017-11-08 17:17:13 -0800

    [fix] edits path to seed data

--- bin/seedDB/config.js
+++ bin/seedDB/config.js
@@ -1,8 +1,8 @@
 import path from 'path';
 import { exec } from 'child_process';
 
-const userSeedCmd = `mongoimport --db ${process.env.MONGO_DB_NAME} --collection users --drop --file ./usersSeedData.json --jsonArray`;
-const roomSeedCmd = `mongoimport --db ${process.env.MONGO_DB_NAME} --collection rooms --drop --file ./roomsSeedData.json --jsonArray`;
+const userSeedCmd = `mongoimport --db ${process.env.MONGO_DB_NAME} --collection users --drop --file ${path.resolve(__dirname, './usersSeedData.json')} --jsonArray`;
+const roomSeedCmd = `mongoimport --db ${process.env.MONGO_DB_NAME} --collection rooms --drop --file ${path.resolve(__dirname, './roomsSeedData.json')} --jsonArray`;
 
 const dbConfigs = {
   seed: {
@@ -12,6 +12,6 @@ const dbConfigs = {
   wipe: {
     data: exec(`mongo ${process.env.MONGO_DB_NAME} --eval "db.dropDatabase()"`)
   }
-}
+};
 
 export default dbConfigs;
